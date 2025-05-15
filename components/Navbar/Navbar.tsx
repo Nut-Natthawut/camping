4ca06e3df9a778b6128d79f342209923f86b7ea4
Natthawut <natthawutkaensao@gmail.com>
2025-05-16 00:00:13 +0700

    01-createnext-setupcomponent-basic

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { DarkMode } from "./darkMode"
+import Logo from "./Logo"
+import Search from "./Search"
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="container flex flex-col justify-between py-8 
+         sm:flex-row sm:items-center gap-4">
+            {/* LOGO */}
+            <Logo />
+            {/* Search */}
+            <Search />
+            {/* Darkmode & profile */}
+            <div className="flex gap-4">
+                <DarkMode />
+                <h1>Profile</h1> 
+            </div>
+           
+        </div>
+    </nav>
+  )
+}
+export default Navbar
