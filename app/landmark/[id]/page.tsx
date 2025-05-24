a8664a6eb5c0807819e5c44a5b015fd253543a02
Natthawut <natthawutkaensao@gmail.com>
2025-05-25 01:12:42 +0700

    16-addMap-Share

--- app/landmark/[id]/page.tsx
+++ app/landmark/[id]/page.tsx
@@ -3,6 +3,8 @@ import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
 import Breadcrums from "@/components/landmark/Breadcrums";
 import Description from "@/components/landmark/Description";
 import ImageContainer from "@/components/landmark/ImageContainer";
+import ShareButton from "@/components/landmark/ShareButton";
+import MapLandmark from "@/components/map/MapLandmark";
 import { redirect } from "next/navigation";
 
 const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
@@ -16,7 +18,7 @@ const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
       <header className="flex justify-between items-center mt-4">
         <h1 className="text-4xl font-bold ">{landmark.name}</h1>
         <div className="flex items-center gap-4">
-          <span>share</span>
+          <ShareButton landmarkId={landmark.id} name={landmark.name}  />
           <FavoriteToggleButton landmarkId={landmark.id} />
         </div>
       </header>
@@ -26,6 +28,9 @@ const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
       <section>
         <div>
           <Description description={landmark.description} />
+          <div className="my-8">
+            <MapLandmark location={{ lat: landmark.lat, lng: landmark.lng }} />
+          </div>
         </div>
       </section>
     </section>
